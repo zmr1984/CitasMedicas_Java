2862d7431aca1d6978fdf3cad3de6eb6a183449b
Zuri <zmr1984@gmail.com>
2026-06-03 08:17:48 -0600

    Implementación continuidad y regeneración de archivos CSV faltantes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ArchivoCSV.inicializarArchivos();
+
         System.out.println("Sistema de Citas Médicas iniciado correctamente.");
 
     }
