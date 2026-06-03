26aa91d43f719cc232fcd22a91d51f996b40838c
Zuri <zmr1984@gmail.com>
2026-06-02 23:41:02 -0600

    Implementación de clase Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,2 +1,35 @@
 public class Doctor {
+
+    private String id;
+    private String nombre;
+    private String especialidad;
+
+    public Doctor(String id,
+                  String nombre,
+                  String especialidad) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public String toString() {
+
+        return id + "," +
+                nombre + "," +
+                especialidad;
+    }
 }
