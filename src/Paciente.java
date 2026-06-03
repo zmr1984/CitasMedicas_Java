f5108555947a72d677be0710c8d3909ad8a7e413
Zuri <zmr1984@gmail.com>
2026-06-02 23:24:27 -0600

    Implementación clase Paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,2 +1,43 @@
 public class Paciente {
+
+    private String id;
+    private String nombre;
+    private String telefono;
+    private int edad;
+
+    public Paciente(String id,
+                    String nombre,
+                    String telefono,
+                    int edad) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+
+        return id + "," +
+                nombre + "," +
+                telefono + "," +
+                edad;
+    }
 }
